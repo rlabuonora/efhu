aa9d6d2c3009289ee1897e77cd49e42894cfd135
Rafa <rlabuonora@yahoo.com>
2015-05-07 15:41:56 -0300

    Added parseFile methods in StataInterface

--- Stata/src/StataInterface.java
+++ Stata/src/StataInterface.java
@@ -1,9 +1,30 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public class StataInterface {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		StdOut.println("Hello Stata!");
+		//StdOut.println("Hello Stata!");
+	}
+	
+	public ArrayList<String[]> parseFile(String file) {
+		ArrayList<String[]> lines = new ArrayList<String[]>();
+		In in = new In(file);
+		// remove headers and determine number of columns+		
+		int cols = in.readLine().split(",").length;
+		
+		String line;
+		while (in.hasNextLine()) {
+			line = in.readLine(); 
+			String[] a = new String[cols];
+			for (int i = 0; i < cols; i++) {
+			a[i] = line.split(",")[i].trim();
+			}
+			lines.add(a);
+		}
+		return lines;
 	}
 
 }
--- Stata/src/StataInterfaceTest.java
+++ Stata/src/StataInterfaceTest.java
@@ -1,13 +1,73 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import org.junit.Test;
 
 
 public class StataInterfaceTest {
 
 	@Test
-	public void callingStdLibWorks() {
-		StdOut.println("Hello!");
+	public void parseQuestionsFileReturnsFiftyTwoLines() {
+		StataInterface si = new StataInterface();
+		String file = "c:\\stata\\preguntas.csv";
+		ArrayList<String[]> questions = si.parseFile(file);
+		assertEquals(52, questions.size());
+	}
+		
+	@Test
+	public void parseQuestionsFirstLineIsFirstLineOfFile() {
+		StataInterface si = new StataInterface();
+		String file = "c:\\stata\\preguntas.csv";
+		ArrayList<String[]> questions = si.parseFile(file);
+		String firstId = "01";
+		String firstVar = "a11";
+		assertEquals(firstId, questions.get(0)[0]);
+		assertEquals(firstVar, questions.get(0)[1]);
+	}
+	
+	
+	@Test
+	public void parseQuestionsLastLineIsFirstLineOfFile() {
+		StataInterface si = new StataInterface();
+		String file = "c:\\stata\\preguntas.csv";
+		ArrayList<String[]> questions = si.parseFile(file);
+		String firstId = "58";
+		String firstVar = "a158";
+		assertEquals(firstId, questions.get(51)[0]);
+		assertEquals(firstVar, questions.get(51)[1]);
+	}
+	
+	@Test
+	public void parseSaltosReturnsSeventyTwoLines() {
+		StataInterface si = new StataInterface();
+		String file = "c:\\stata\\saltos.csv";
+		ArrayList<String[]> saltos = si.parseFile(file);
+		assertEquals(75, saltos.size());
+	}
+	
+	
+	@Test
+	public void parseSaltosLastLineIsFirstLineOfFile() {
+		StataInterface si = new StataInterface();
+		String file = "c:\\stata\\saltos.csv";
+		ArrayList<String[]> saltos = si.parseFile(file);
+		String firstId = "01";
+		String firstVar = "-1";
+		assertEquals(firstId, saltos.get(0)[0]);
+		assertEquals(firstVar, saltos.get(0)[1]);
+	}
+	
+	@Test
+	public void parseSaltosLastLineIsLastLineOfFile() {
+		StataInterface si = new StataInterface();
+		String file = "c:\\stata\\saltos.csv";
+		ArrayList<String[]> saltos = si.parseFile(file);
+		String firstId = "58";
+		String firstVar = "-1";
+		assertEquals(firstId, saltos.get(74)[0]);
+		assertEquals(firstVar, saltos.get(74)[1]);
 	}
 	
 

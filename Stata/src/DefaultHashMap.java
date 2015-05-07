81cd823f0501bef0aa58880dcfb41e6597502918
Rafa <rlabuonora@yahoo.com>
2015-05-07 11:05:29 -0300

    DefaultHashMap constructor takes no arguments

--- Stata/src/DefaultHashMap.java
+++ Stata/src/DefaultHashMap.java
@@ -4,9 +4,7 @@ public class DefaultHashMap<K,V> extends HashMap<K,V> {
     
     protected V defaultValue;
     
-    public DefaultHashMap(V defaultValue) {
-        this.defaultValue = defaultValue;
-    }
+
 
     @Override
     public V get(Object k) {
--- Stata/src/DefaultHashMapTest.java
+++ Stata/src/DefaultHashMapTest.java
@@ -7,21 +7,22 @@ public class DefaultHashMapTest {
 
 	@Test
 	public void emptyHashReturnsDefaultValue() {
-		 DefaultHashMap<Double, String> dict = new DefaultHashMap<Double, String>("not found");
-	     assertEquals(dict.get(1.0), "not found");
+		 DefaultHashMap<Double, String> dict = new DefaultHashMap<Double, String>();
+	     dict.setDefault("not found");
+		 assertEquals(dict.get(1.0), "not found");
 
 	}
 	
 	@Test
 	public void oneElementHashReturnsElement() {
-		 DefaultHashMap<Double, String> dict = new DefaultHashMap<Double, String>("not found");
+		 DefaultHashMap<Double, String> dict = new DefaultHashMap<Double, String>();
 	     dict.put(1.0, "uno");
 	     assertEquals(dict.get(1.0), "uno");
 	}
 	
 	@Test
 	public void changeDefaultChangesNotFoundReturnValue() {
-		 DefaultHashMap<Double, String> dict = new DefaultHashMap<Double, String>("not found");
+		 DefaultHashMap<Double, String> dict = new DefaultHashMap<Double, String>();
 		 dict.put(2.0, "dos");
 		 dict.setDefault("99");
 	     assertEquals(dict.get(1.0), "99");

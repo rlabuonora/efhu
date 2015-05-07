c497407aa91b0a77a5e94e0a16efdf9e3c065d4c
Rafa <rlabuonora@yahoo.com>
2015-05-07 10:32:26 -0300

    Initial commit

---
+++ Stata/src/StataInterface.java
@@ -0,0 +1,9 @@
+
+public class StataInterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StdOut.println("Hello Stata!");
+	}
+
+}
---
+++ Stata/src/StataInterfaceTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class StataInterfaceTest {
+
+	@Test
+	public void callingStdLibWorks() {
+		StdOut.println("Hello!");
+	}
+	
+
+	
+	
+
+}
